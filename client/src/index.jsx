8bebf292a5970117228b08fbda73067a5ee337c2
Cameron Montgomery <jc.montgomery0102@gmail.com>
2022-11-19 13:59:01 -0700

    removed firestore analytics

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import * as firebase from 'firebase';
+
 import App from './components/App';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDMQiT_igpgOF7V98WiDjiCbsk0W4ciWA0",
+  authDomain: "mine-yours.firebaseapp.com",
+  projectId: "mine-yours",
+  storageBucket: "mine-yours.appspot.com",
+  messagingSenderId: "151804095653",
+  appId: "1:151804095653:web:5789d4eb641f2035bf314c",
+  measurementId: "G-PH0YQ7F1QJ"
+};
+
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(<App />);
