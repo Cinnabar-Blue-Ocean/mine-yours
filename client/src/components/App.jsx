8a836bf4126ad0241e1bb19b754b82a1ce35443b
xincheng zhou <prifvibea@gmail.com>
2022-11-19 18:09:58 -0600

    add router

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
-import LandingPage from './Landing Page/LandingPage'
-import Navbar from './navbar/Navbar.jsx';
+// import LandingPage from './Landing Page/LandingPage'
+// import Navbar from './navbar/Navbar.jsx';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+import Home from '../pages/Home.jsx'
+import SignIn from '../pages/SignIn.jsx'
+import SignUp from '../pages/SignUp.jsx'
+import Profile from '../pages/Profile.jsx'
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <LandingPage />
-    </div>
+    <Box>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile/:userId" element={<Profile />} />
+        <Route path="/signIn" element={<SignIn />} />
+        <Route path="/signUp" element={<SignUp />} />
+      </Routes>
+    </Box>
   );
 };
 
 export default App;
+
+
+
