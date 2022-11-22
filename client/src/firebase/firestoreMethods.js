25d040776bc3cb95b5341b0a1858a792bdefb66e
Cameron Montgomery <jc.montgomery0102@gmail.com>
2022-11-22 12:49:22 -0700

    Stash to checkout

--- client/src/firebase/firestoreMethods.js
+++ client/src/firebase/firestoreMethods.js
@@ -36,8 +36,17 @@ const getTradeById = (trade_id) => {
 //post a review
 
 //post a trade
-const postTrade = (listing_id, ) => {
+const postTrade = async (listing_id, receiver_id, start_date, expiration_date) => {
+  try {
+    const listing = await getListingById(listing_id)
+    console.log(listing)
+    let docRef = await addDoc(collection(db, 'trades'), {
+      listing_id,
+      owner_id
+    })
+  } catch (err) {
 
+  }
 }
 
 //post a listing
