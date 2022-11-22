5249ea5ea54378ae1c288a3472f8d9686778f039
Nick Amenda <104174057+nickamenda@users.noreply.github.com>
2022-11-22 15:53:27 -0600

    fix loader error

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -14,6 +14,7 @@ import {
   getMessages,
   getReviews,
   getTrades } from '../firebase/retrieveData';
+import Loader from '../pages/Loading.jsx'
 
 
 const App = () => {
@@ -72,7 +73,7 @@ const App = () => {
         <Route path="/signIn" element={<SignIn />} />
         <Route path="/signUp" element={<SignUp />} />
         <Route path="/userInfo" element={<CollectUserInfo />} />
-        <Route path="/loading" element={<Loading />} />
+        <Route path="/loading" element={<Loader />} />
       </Routes>
     </Box>
   );
