d569bbf6924a1c257fec5faad7ea3623b80cac44
Nick Amenda <104174057+nickamenda@users.noreply.github.com>
2022-11-22 16:46:05 -0600

    test adding photos

--- client/src/components/Landing Page/LandingPage.jsx
+++ client/src/components/Landing Page/LandingPage.jsx
@@ -10,7 +10,7 @@ const LandingPage = () => {
   const [listings, setListings] = useState([{
     description: 'testteststest. Thjis is a test blag blahj',
     name: 'testteststest',
-    photos: ['https://plus.unsplash.com/premium_photo-1661270415179-f7bcff006edb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmFyYmVyc2hvcHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'],
+    photos: ['https://plus.unsplash.com/premium_photo-1661270415179-f7bcff006edb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmFyYmVyc2hvcHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60','https://plus.unsplash.com/premium_photo-1661270415179-f7bcff006edb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmFyYmVyc2hvcHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60','https://plus.unsplash.com/premium_photo-1661270415179-f7bcff006edb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmFyYmVyc2hvcHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60','https://plus.unsplash.com/premium_photo-1661270415179-f7bcff006edb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmFyYmVyc2hvcHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'],
     type: 'testteststest',
     status: 'testteststest',
     user_id: 'nickamenda',
@@ -85,7 +85,7 @@ const LandingPage = () => {
     // .then((data) => {
     //   setListings(data);
     // })
-    // findDistance(61080, 61073)
+    findDistance(61080, 61073)
   }, [])
 
   return listings.length > 0 ? (
