5cc6b913c337863f7b24fa8510c2f5a09e7e8792
Nick Amenda <104174057+nickamenda@users.noreply.github.com>
2022-11-23 10:39:43 -0600

    push up my changes

--- client/src/components/Landing Page/LandingPage.jsx
+++ client/src/components/Landing Page/LandingPage.jsx
@@ -79,19 +79,27 @@ const LandingPage = () => {
     status: 'testteststest',
     user_id: 'nickamenda',
     zip_code: 61080
+  },{
+    description: 'testteststest',
+    name: 'testteststest',
+    photos: ['https://plus.unsplash.com/premium_photo-1661270415179-f7bcff006edb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmFyYmVyc2hvcHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'],
+    type: 'testteststest',
+    status: 'testteststest',
+    user_id: 'nickamenda',
+    zip_code: 61080
   }])
   useEffect(() => {
-    // getListings()
-    // .then((data) => {
-    //   setListings(data);
-    // })
-    findDistance(61080, 61073)
+    getListings()
+    .then((data) => {
+      setListings(data);
+    })
+    // findDistance(61080, 61073)
   }, [])
 
   return listings.length > 0 ? (
     <>
       <div id="landing-page">
-        <Sidebar />
+        <Sidebar setListings={setListings}/>
         <div id="trade-container">
           <Trades listings={listings}/>
         </div>
